02db0b7763b9ef582b46e6416ac484a852521ac6
Plops013 <pecorafatec@gmail.com>
2020-05-15 12:09:37 -0300

    Commit inicial
    todos os componentes gerados, só falta a mão na massa agora kkk

---
+++ src/app/casa-de-show/casa-de-show.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ListarComponent } from './listar/listar.component';
+
+
+
+@NgModule({
+  declarations: [CadastrarComponent, ListarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CasaDeShowModule { }
---
+++ src/app/evento/evento.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+
+
+@NgModule({
+  declarations: [CadastrarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EventoModule { }
